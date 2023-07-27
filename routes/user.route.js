0ce55be20c3e7908f4cb4f1bce0d1a938004c27d
Rajneesh Shukla <124131361+developerrajneesh@users.noreply.github.com>
2023-07-27 18:08:39 +0530

    Add files via upload

---
+++ routes/user.route.js
@@ -0,0 +1,19 @@
+import express from "express"
+import { signUp, verify,signIn,addEducation,sendForgotOtp, addEmployement,setNewPassword,loggedInData,userUpdate} from "../controllers/user.controller.js"
+import auth from "../utils/auth.js"
+import multerUpload from "../utils/multerUpload.js"
+
+const router = express.Router()
+
+router.post("/signup",multerUpload.single('resume'), signUp)
+router.post("/signin", signIn)
+router.post("/send-forgot-otp", sendForgotOtp)
+router.post("/add-employment",auth, addEmployement)
+router.post("/add-education",auth, addEducation)
+router.post("/set-new-password", setNewPassword)
+router.post("/user-update",multerUpload.single('document'), userUpdate)
+router.post("/verify", verify)
+router.get("/get-loggedIn-data", auth, loggedInData)
+
+
+export default router
